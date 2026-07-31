bc2ec8462b27636f1ea4ea7ff273c71ef24a0319
Nikita Vasilev <nikitas2000@mail.ru>
2026-07-31 21:15:32 +0300

    Changed

--- yacut/yandex_disk.py
+++ yacut/yandex_disk.py
@@ -31,16 +31,19 @@ async def get_upload_url(filename):
 async def upload_file(file, filename):
     upload_url = await get_upload_url(filename)
 
+    file.stream.seek(0)
+    file_data = file.stream.read()
+
     async with aiohttp.ClientSession() as session:
         async with session.put(
             upload_url,
-            data=file.stream
+            data=file_data
         ) as response:
             response.raise_for_status()
-
-            location = response.headers["Location"]
+            location = response.headers['Location']
             location = urllib.parse.unquote(location)
-            return location.replace("/disk", "")
+            location = location.replace('/disk', '')
+            return location
 
 
 async def get_download_link(path):
